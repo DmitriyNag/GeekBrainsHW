dfd4a88337adf065ee85bd24413cee781b042433
Dmitriy <dnagovicin@gmail.com>
2018-10-01 19:10:36 +0500

    Выполнено 2е ДЗ

--- lesson-4 HW/lesson-4 HW/Program.cs
+++ lesson-4 HW/lesson-4 HW/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +9,36 @@ namespace lesson_4_HW
     {
         static void Main(string[] args)
         {
+            #region 1 задание
+            MyArray arr = new MyArray(20);
+            arr.InitArray(-10000, 10000);
+            Console.WriteLine(arr.PrintArray());
+            Console.WriteLine("Кол-во пар, в которых хотя бы одно число /3: " + arr.CountPairDivTree());
+            MyClass.Pause();
+            #endregion
+
+
+            #region Задание 2
+            MyArray mylist = new MyArray(10, -15, 5);
+            Console.WriteLine(mylist.PrintArray());
+            Console.WriteLine("Кол-во пар, в которых хотя бы одно число /3: " + mylist.CountPairDivTree());
+            mylist.Inverse();
+            Console.WriteLine("Инвертировали знаки: \n" + mylist.PrintArray());
+            mylist.Multi(-5);
+            Console.WriteLine("Умножили: \n" + mylist.PrintArray());
+            MyClass.Pause();
+            Console.WriteLine(mylist.Sum);
+            Console.WriteLine(mylist.MaxCount);
+            MyClass.Pause();
+            MyArray listFromFile = new MyArray("input.txt");
+            Console.WriteLine(listFromFile.PrintArray());
+            listFromFile.Multi(-5);
+            listFromFile.PrintToFile("output.txt");
+            MyClass.Pause(); 
+            #endregion
+
         }
+
+
     }
 }
