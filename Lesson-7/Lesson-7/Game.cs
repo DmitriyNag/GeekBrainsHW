642c56498ffdd147e8f9872430fd6132a084213d
Dmitriy <dnagovicin@gmail.com>
2018-10-13 17:50:07 +0500

    Выполнена задача 2, решил добавить визуальное отображние диапазона числе, и после ответа игрока визуально подсвечивать оставшуюся часть диапазона ( т.е. в которой может быть загаданное число).

---
+++ Lesson-7/Lesson-7/Game.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Lesson_7
+{
+    class Game
+    {
+        public bool IsStarted { get; set; }
+        public int RightAnswer { get; set; }
+        public int CountSteps { get; set; }
+        public int RangeStart { get; set; }
+        public int RangeEnd { get; set; }
+        public void Start(int a, int b)
+        {
+            IsStarted = true;
+            Random rnd = new Random();
+            RangeStart = a;
+            RangeEnd = b;
+            RightAnswer = rnd.Next(RangeStart, RangeEnd);
+            CountSteps = 0;
+        }
+
+        /// <summary>
+        /// проверяем верность ответа. возвращаем
+        ///     2, если ответ больше верного,
+        ///     1 если ответ меньше верного,
+        ///     0 - если ответ совпал,
+        ///     -1 - если не удвлось считать ответ
+        /// </summary>
+        /// <param name="answerString"></param>
+        /// <returns></returns>
+        public int CheckAnswer(string answerString)
+        {
+            if (int.TryParse(answerString, out int answer))
+            {
+                CountSteps++;
+                if (answer > RightAnswer) return 2;
+                else if (answer < RightAnswer) return 1;
+                else 
+                {
+                    IsStarted = false;
+                    return 0;
+                }
+            }
+            else return -1;
+        }
+
+        /// <summary>
+        /// возвращаем точку на шкале ответов, в процентах от мин до макс возможного ответа
+        /// </summary>
+        /// <param name="answerString"></param>
+        /// <returns></returns>
+        public double GetAnswerPosition(string answerString)
+        {
+            if (int.TryParse(answerString, out int answer))
+            {
+                return Math.Round((double)answer/RangeEnd,2);
+            }
+            else return 0;
+        }
+    }
+}
